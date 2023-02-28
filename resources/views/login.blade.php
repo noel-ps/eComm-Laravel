5f0d39a5103aef6c901223a2e505a22eb337974e
unknown <noelmaliganya1@gmail.com>
2023-02-28 16:29:58 +0300

    header, footer and login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+@section('content')
+
+
+{{-- <div class="container">
+    <div class="row">
+        <div class="col mb-4"> --}}
+            <section class="vh-100" style="background-color: #9A616D;">
+                <div class="container py-5 h-100">
+                  <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col col-xl-8 align-items-center">
+                      <div class="card" style="border-radius: 1rem;">
+                        <div class="row g-0">
+                          <div class="col-md-6 col-lg-5 d-none d-md-block">
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                              alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                          </div>
+                          <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                            <div class="card-body p-4 p-lg-5 text-black">
+              
+                              <form>
+              
+                                <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+              
+                                <div class="form-outline mb-4">
+                                  <label class="form-label" for="form2Example17">Email address</label>
+                                  <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                                </div>
+              
+                                <div class="form-outline mb-4">
+                                <label class="form-label" for="form2Example27">Password</label>
+                                <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                                </div>
+              
+                                <div class="pt-1 mb-4">
+                                  <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                                </div>
+                                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!"
+                                    style="color: #393f81;">Register here</a></p>
+                               
+                              </form>
+              
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+        {{-- </div>
+    </div>
+</div> --}}
+
+@endsection
